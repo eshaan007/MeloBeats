dd404d9c26bbcd0f04791193e89e0fd7317a31bf
eshaan007 <eshaany2k@gmail.com>
2020-11-10 13:22:54 +0530

    Basic Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Player from './Components/Player';
+import Song from './Components/Song';
+
 function App() {
   return (
     <div className="App">
       <h1> MeloBeats </h1>
+      <Song />
+      <Player />
     </div>
   );
 }
---
+++ src/Components/Song.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Song = () => {
+    return(
+        <div className="song-container">
+            <h1> Picture </h1>
+            <h1> Song </h1>
+            <h1> Artists </h1>
+        </div>
+    );
+}
+
+export default Song;
