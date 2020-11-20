66343cfe744eb59b5af1682a8886f388668a9374
eshaan007 <eshaany2k@gmail.com>
2020-11-20 15:34:00 +0530

    Pause Play Icon Inserted

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,6 +1,11 @@
 import React, {useRef, useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { 
+    faPlay, 
+    faAngleLeft, 
+    faAngleRight,
+    faPause,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     // state
@@ -34,8 +39,8 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     }
 
     const [songInfo, setSongInfo] = useState ({
-        currentTime: null,
-        duration: null,
+        currentTime: 0,
+        duration: 0,
     });
 
     return(
@@ -54,7 +59,11 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
+                <FontAwesomeIcon 
+                    onClick={playSongHandler} 
+                    className="play" 
+                    size="2x" 
+                    icon={isPlaying ? faPause : faPlay} />
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
             </div>
 
