edc1e90ff85414a42954d407aa56aa60bc10e254
eshaan007 <eshaany2k@gmail.com>
2020-11-23 00:37:22 +0530

    Color Bar

--- src/App.js
+++ src/App.js
@@ -17,12 +17,23 @@ function App() {
   const [songInfo, setSongInfo] = useState ({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
 });
 const [libraryStatus, setLibraryStatus] = useState(false); 
 const timeUpdateHandler = (e) => {
   const current = e.target.currentTime;
   const duration = e.target.duration;
-  setSongInfo({...songInfo, currentTime: current, duration})
+  //Calculate Percentage
+  const roundedCurrent = Math.round(current);
+  const roundedDuration = Math.round(duration);
+  const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+
+  setSongInfo({
+    ...songInfo, 
+    currentTime: current, 
+    duration, 
+    animationPercentage: animation,
+  });
 };
 
   return (
